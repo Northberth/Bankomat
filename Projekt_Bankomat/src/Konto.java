1538b70c3637ee9cdf4ad4c3350290a97bc6664f
Norbert Mielniczuk <norbertm398@gmail.com>
2021-06-11 18:26:43 +0200

    Obsługa wypłat i prób logowania

--- Projekt_Bankomat/src/Konto.java
+++ Projekt_Bankomat/src/Konto.java
@@ -35,4 +35,26 @@ public class Konto {
 	public void add20() {
 		balance += 20;
 	}
+	public void removeTry() {
+		if(tries > 0) {
+			tries -= 1;
+		}	
+	}
+	public void successfulLogin() {
+		tries = 3;
+	}
+	public boolean withdraw(double amount) {
+		if(amount > balance) {
+			System.out.println("Brak wystarczaj¹cej iloœci œrodków na koncie");
+		} else if(amount < 50) {
+			System.out.println("Minimalne mo¿na wyp³aciæ 50 z³");
+		} else if(amount%10==0) {
+			balance -= amount;
+			System.out.println("Wyp³ata zakoñczona sukcesem");
+			return true;
+		} else {
+			System.out.println("Kwota wyp³aty musi byæ wielokrotnoœci¹ banknotu 10 z³");
+		}
+		return false;
+	}
 }
