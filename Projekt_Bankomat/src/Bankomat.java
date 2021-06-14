f3ed17b85df2416338adc864ba3dacc486aabb66
Norbert Mielniczuk <norbertm398@gmail.com>
2021-06-14 18:41:05 +0200

    Działające numeryczne przyciski

--- Projekt_Bankomat/src/Bankomat.java
+++ Projekt_Bankomat/src/Bankomat.java
@@ -14,7 +14,14 @@ public class Bankomat implements ActionListener {
 	JFrame f;
 	JTextField t;
 	JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bok,blogout,blogin,binfo,bbalance,bwithdraw,bclear,bcancel;
+	static boolean isLogged;
+	private static Konto loggedUser;
+	JLabel linfo, lamount;
+	String amount;
 	public Bankomat() {
+		isLogged = false;
+		loggedUser = null;
+		amount = "";
 		f = new JFrame("Bankomat");
 		//t = new JTextField();
 		binfo = new JButton("Info");
@@ -31,6 +38,12 @@ public class Bankomat implements ActionListener {
 		bclear = new JButton("Clear");
 		bok = new JButton("OK");
 		bcancel = new JButton("Cancel");
+		blogout = new JButton("Logout");
+		blogin = new JButton("Login");
+		bbalance = new JButton("Balance");
+		bwithdraw = new JButton("Withdraw");
+		linfo = new JLabel("Witaj w bankomacie");
+		lamount = new JLabel("");
 		f.setLayout(null);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,19 +52,25 @@ public class Bankomat implements ActionListener {
 		//t.setBounds(30, 40, 280, 30);
 		//f.add(t);
 		binfo.setBounds(310, 15, 60, 30);
-		b0.setBounds(155, 500, 50, 40);
-		b1.setBounds(95, 350, 50, 40);
-		b2.setBounds(155, 350, 50, 40);
-		b3.setBounds(215, 350, 50, 40);
-		b4.setBounds(95, 400, 50, 40);
-		b5.setBounds(155, 400, 50, 40);
-		b6.setBounds(215, 400, 50, 40);
-		b7.setBounds(95, 450, 50, 40);
-		b8.setBounds(155, 450, 50, 40);
-		b9.setBounds(215, 450, 50, 40);
-		bok.setBounds(285, 450, 80, 40);
-		bclear.setBounds(285, 400, 80, 40);
-		bcancel.setBounds(285, 350, 80, 40);
+		b0.setBounds(170, 500, 50, 40);
+		b1.setBounds(110, 350, 50, 40);
+		b2.setBounds(170, 350, 50, 40);
+		b3.setBounds(230, 350, 50, 40);
+		b4.setBounds(110, 400, 50, 40);
+		b5.setBounds(170, 400, 50, 40);
+		b6.setBounds(230, 400, 50, 40);
+		b7.setBounds(110, 450, 50, 40);
+		b8.setBounds(170, 450, 50, 40);
+		b9.setBounds(230, 450, 50, 40);
+		bok.setBounds(290, 450, 90, 40);
+		bclear.setBounds(290, 400, 90, 40);
+		bcancel.setBounds(290, 350, 90, 40);
+		blogout.setBounds(5, 400, 90, 40);
+		blogin.setBounds(5, 350, 90, 40);
+		bbalance.setBounds(5, 450, 90, 40);
+		bwithdraw.setBounds(150, 300, 90, 40);
+		linfo.setBounds(5, 50, 600, 40);
+		lamount.setBounds(5, 70, 600, 40);
 		f.add(binfo);
 		f.add(b0);
 		f.add(b1);
@@ -66,6 +85,12 @@ public class Bankomat implements ActionListener {
 		f.add(bok);
 		f.add(bclear);
 		f.add(bcancel);
+		f.add(blogout);
+		f.add(blogin);
+		f.add(bbalance);
+		f.add(bwithdraw);
+		f.add(linfo);
+		f.add(lamount);
 		binfo.addActionListener(this);
 		b0.addActionListener(this);
 		b1.addActionListener(this);
@@ -80,6 +105,10 @@ public class Bankomat implements ActionListener {
 		bok.addActionListener(this);
 		bclear.addActionListener(this);
 		bcancel.addActionListener(this);
+		blogout.addActionListener(this);
+		blogin.addActionListener(this);
+		bbalance.addActionListener(this);
+		bwithdraw.addActionListener(this);
 	}
 	public static void main(String[] args) {
 		plik = new File();
@@ -104,12 +133,13 @@ public class Bankomat implements ActionListener {
 				if (account.getTries() <= 0) {
 					System.out.println("Konto zablokowane. Proszê siê skontaktowaæ z administratorem");
 				}
-				else if(account.getPin() == pin) {
+				else if(account.getPin().equals(pin)) {
 					System.out.println("Zalogowano pomyœlnie");
 					System.out.println("Witaj " + account.getName());
 					account.successfullLogin();
 					plik.SaveFile(konta);
-					userMenu(account);
+					isLogged = true;
+					loggedUser = account;
 					return true;
 				} else {
 					account.removeTry();
@@ -146,6 +176,39 @@ public class Bankomat implements ActionListener {
 			f1.add(label1);
 			f1.add(label2);
 			f1.add(label3);
+		} else if(e.getSource() == b0) {
+			amount += "0";
+			lamount.setText(amount);
+		} else if(e.getSource() == b1) {
+			amount += "1";
+			lamount.setText(amount);
+		}else if(e.getSource() == b2) {
+			amount += "2";
+			lamount.setText(amount);
+		}else if(e.getSource() == b3) {
+			amount += "3";
+			lamount.setText(amount);
+		} else if(e.getSource() == b4) {
+			amount += "4";
+			lamount.setText(amount);
+		}else if(e.getSource() == b5) {
+			amount += "5";
+			lamount.setText(amount);
+		}else if(e.getSource() == b6) {
+			amount += "6";
+			lamount.setText(amount);
+		} else if(e.getSource() == b7) {
+			amount += "7";
+			lamount.setText(amount);
+		}else if(e.getSource() == b8) {
+			amount += "8";
+			lamount.setText(amount);
+		}else if(e.getSource() == b9) {
+			amount += "9";
+			lamount.setText(amount);
+		}else if(e.getSource() == bclear) {
+			amount = "";
+			lamount.setText(amount);
 		}
 	}
 }
